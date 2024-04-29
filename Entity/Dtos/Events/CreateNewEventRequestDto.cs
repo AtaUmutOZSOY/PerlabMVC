3ded20f001d6a90a09c64423b17b42abb6d4b2a5
Ata Umut OZSOY <37904413+AtaUmutOZSOY@users.noreply.github.com>
2024-04-29 18:45:39 +0300

    Last Updates

---
+++ Entity/Dtos/Events/CreateNewEventRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Dtos.Events
+{
+    public class CreateNewEventRequestDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string EventLink { get; set; }
+        public DateTime EventTime { get; set; }
+
+    }
+}
--- WebAPI/Controllers/EventsController.cs
+++ WebAPI/Controllers/EventsController.cs
@@ -1,6 +1,9 @@
 ﻿using Business.Abstract;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Models.Concrete;
+using Models.Dtos.Events;
+using Models.Dtos.NewsFeed;
 
 namespace WebAPI.Controllers
 {
@@ -22,6 +25,33 @@ namespace WebAPI.Controllers
         {
             var result = _newsFeedService.GetAllEvents();
             return Ok(result); 
+        }
+
+        [HttpPost("createNewEvent")]
+
+        public IActionResult CreateNewEvent(CreateNewsFeedRequestDto createNewsFeedRequestDto)
+        {
+
+            var result = _newsFeedService.CreateNewsFeed(createNewsFeedRequestDto);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+
+        }
+
+        [HttpDelete("deleteEventById/{id}")]
+
+        public IActionResult DeleteById(int id)
+        {
+            var result = _newsFeedService.DeleteNewsFeedById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
         }
     }
 }
